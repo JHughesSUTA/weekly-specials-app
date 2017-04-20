4d9e590d94294e9a4383923475135848c1093b5f
James Hughes <james.a.hughes@gmail.com>
2017-04-19 19:45:03 -0500

    add faker gem, remove username from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420000704) do
+ActiveRecord::Schema.define(version: 20170420004305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20170420000704) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "username"
     t.string   "password_digest"
     t.boolean  "admin"
     t.datetime "created_at",      null: false
